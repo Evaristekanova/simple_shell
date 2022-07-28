08eb5ebda39bcf24820cf8816e98ad2de2ef5a7d
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-28 14:39:30 +0300

    beautiful code that passes the Betty checks

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+  * main - check the code
+  * Return: Always success (0)
+  */
+  
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+void rev_string(char *s);
+
+
+
+
+
+
+
+#endif /* SHELL_H */
