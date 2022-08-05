f567ee9ce36c0573e46b36929082fdaefb57548b
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-08-05 22:15:36 +0300

    function that splits string entered by user

--- shell.h
+++ shell.h
@@ -1,10 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
+#define bufsize 1024
+
 /* standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+
+
 
 
 
---
+++ str_array.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+* str_array - function that splits string entered by user
+* @buffer: the string to be split
+* Return: the sub-strings of the split string
+*/
+
+char **str_array(char *buffer)
+{
+	char **commands = NULL;
+	char *token, *delimeter = " \n";
+	int i;
+
+	commands= malloc(sizeof(char *) * bufsize);
+	if (commands == NULL)
+	{
+		perror("memory could not be allocated");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(buffer, delimeter);
+	i = 0;
+	while (token != NULL)
+	{
+		commands[i] = token;
+		token = strtok(NULL, delimeter);
+		i++;
+	}
+	commands[i] = NULL;
+	return (commands);
+}
